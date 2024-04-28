d5f677dd66d3185a1c34cd40a211268600b64bbd
Alex <ruben.ramos51@uabc.edu.mx>
2024-04-27 19:07:57 -0700

    PlantProvider implementado.

--- app/src/main/java/com/example/sunflowerapp/DashboardActivity.kt
+++ app/src/main/java/com/example/sunflowerapp/DashboardActivity.kt
@@ -1,22 +1,24 @@
 package com.example.sunflowerapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.sunflowerapp.screens.DashboardScreen
 import com.example.sunflowerapp.ui.theme.SunflowerAppTheme
+import com.example.sunflowerapp.utils.RepositoryProvider
 
 class DashboardActivity : ComponentActivity() {
+    private val plantRepository: PlantRepository by lazy {
+        RepositoryProvider.providePlantRepository(applicationContext)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        val plantRepository = PlantRepository(this)
+        //val plantRepository = PlantRepository(this)
         val listaDePlantas = plantRepository.getPlants()
         var listaDeJardin = plantRepository.getGardenPlants()
 
@@ -29,25 +31,16 @@ class DashboardActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DashboardScreen( listOfPlants = listaDePlantas, listOfGardenPlants = listaDeJardin)
+
+                    DashboardScreen( listOfPlants = listaDePlantas, listOfGardenPlants = listaDeJardin,
+                        onClick = { plant ->
+                            val intent = Intent(this, PlantDetailActivity::class.java)
+                            intent.putExtra("plant", plant) // Pass the plant object as an extra
+                            startActivity(intent)
+                        })
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting3(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview3() {
-    SunflowerAppTheme {
-        Greeting3("Android")
     }
 }
