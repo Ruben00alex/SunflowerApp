ca98cdc5c5b1bf0e2a3f9d0a17bd11e166d77333
Alex <ruben.ramos51@uabc.edu.mx>
2024-04-28 23:13:17 -0700

    water plant

--- app/src/main/java/com/example/sunflowerapp/PlantRepository.kt
+++ app/src/main/java/com/example/sunflowerapp/PlantRepository.kt
@@ -10,43 +10,49 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-class PlantRepository(private val context: Context) { // Store context in a property
+class PlantRepository(private val context: Context) {
     private val plants: List<Plant>
     private var gardenPlants: MutableList<Plant>
+
     init {
         plants = readPlantsJson(context)
         gardenPlants = readGardenJson(context).toMutableList()
     }
+    fun waterPlantById(plantId: String): List<Plant> {
+        val plantIndex = gardenPlants.indexOfFirst { it.id == plantId }
+        if (plantIndex != -1) {
+            val updatedPlant = gardenPlants[plantIndex].copy(lastWateredDate = getCurrentDate())
 
+            val newGardenPlants = gardenPlants.toMutableList()
+            newGardenPlants[plantIndex] = updatedPlant
+
+            // Update the JSON file with the new list
+            writeGardenJson(context, newGardenPlants)
+
+            gardenPlants = newGardenPlants
+
+            return newGardenPlants.toList()
+        } else {
+            return gardenPlants.toList()
+        }
+    }
     fun getPlants(): List<Plant> = plants
 
     fun getGardenPlants(): List<Plant> = gardenPlants
 
-    fun addPlantToGarden(plant: Plant) : List<Plant> {
-
-        //we have to change the conditional, because we are now comparing the whole plant object
-        //so we check the id of the plant object
-        if(gardenPlants.any { it.id == plant.id }){
+    fun addPlantToGarden(plant: Plant): List<Plant> {
+        if (gardenPlants.any { it.id == plant.id }) {
             Log.println(Log.INFO, "PlantRepository", "Plant already in garden")
             return gardenPlants
         }
 
-        //Print number of plants in the garden before adding
-        Log.println(Log.INFO, "PlantRepository", gardenPlants.size.toString())
-
-
         val plantWithDate = plant.copy(plantedDate = getCurrentDate())
         gardenPlants.add(plantWithDate)
         writeGardenJson(context, gardenPlants) // context is now accessible
-        //Print number of plants in the garden after adding
-        Log.println(Log.INFO, "PlantRepository", gardenPlants.size.toString())
-        Log.println(Log.INFO, "PlantRepository", "Plant added to garden")
-        //Print name of the plant
-        Log.println(Log.INFO, "PlantRepository", plant.name)
         return gardenPlants
     }
 
-    fun removePlantFromGarden(plant: Plant) : List<Plant> {
+    fun removePlantFromGarden(plant: Plant): List<Plant> {
         //we will remove by id:
         gardenPlants = gardenPlants.filter { it.id != plant.id }.toMutableList()
         Log.println(Log.INFO, "PlantRepository", "Plant removed from garden")
@@ -60,6 +66,7 @@ class PlantRepository(private val context: Context) { // Store context in a prop
     fun checkIfPlantInGarden(plant: Plant): Boolean {
         return gardenPlants.any { it.id == plant.id }
     }
+
     private fun getCurrentDate(): String {
         val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         return dateFormat.format(Date())
@@ -75,16 +82,13 @@ class PlantRepository(private val context: Context) { // Store context in a prop
     }
 
     private fun readGardenJson(context: Context): List<Plant> {
-        //val gardenJson = context.assets.open("myGarden.json").bufferedReader().use { it.readText() }
-
-        //gardenJSON will be a persistent file in the app's internal storage, so we have to first check if the file exists
-        //if not, we create an empty file.
         val gardenFile = context.getFileStreamPath("myGarden.json")
 
         val gardenJson = if (gardenFile.exists()) {
             context.openFileInput("myGarden.json").bufferedReader().use { it.readText() }
         } else {
-            context.openFileOutput("myGarden.json", Context.MODE_PRIVATE).use { it.write("".toByteArray()) }
+            context.openFileOutput("myGarden.json", Context.MODE_PRIVATE)
+                .use { it.write("".toByteArray()) }
             ""
         }
 
@@ -97,15 +101,17 @@ class PlantRepository(private val context: Context) { // Store context in a prop
 
 
     // reset the garden and write the empty list to the file
-    fun resetGarden() : List<Plant>{
+    fun resetGarden(): List<Plant> {
         gardenPlants = mutableListOf()
         writeGardenJson(context, gardenPlants)
         return gardenPlants
     }
+
     private fun writeGardenJson(context: Context, gardenPlants: List<Plant>) {
         println("writeGardenJson")
         val jsonString = Json.encodeToString(gardenPlants)
         //write the json string to the file in the internal storage:
-        context.openFileOutput("myGarden.json", Context.MODE_PRIVATE).use { it.write(jsonString.toByteArray()) }
+        context.openFileOutput("myGarden.json", Context.MODE_PRIVATE)
+            .use { it.write(jsonString.toByteArray()) }
     }
 }
--- app/src/main/java/com/example/sunflowerapp/composables/PlantGardenCard.kt
+++ app/src/main/java/com/example/sunflowerapp/composables/PlantGardenCard.kt
@@ -1,9 +1,12 @@
 package com.example.sunflowerapp.composables
+
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,24 +16,33 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.example.sunflowerapp.models.Plant
+import com.example.sunflowerapp.viewmodels.GardenViewModel
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun PlantCardGarden(
-    plant: Plant,
-    modifier: Modifier = Modifier
+    plantID: String,
+    modifier: Modifier = Modifier,
+    viewModel: GardenViewModel,
 ) {
+    val plant = viewModel.getPlant(plantID)
+    if (plant == null) {
+        Log.e("PlantCardGarden", "Plant not found")
+        return
+    }
+
     OutlinedCard(
         modifier = modifier,
         colors = CardDefaults.outlinedCardColors(
@@ -39,18 +51,18 @@ fun PlantCardGarden(
         border = BorderStroke(0.dp, MaterialTheme.colorScheme.outline)
     ) {
         Column(
-            modifier = Modifier.padding(0.dp )
+            modifier = Modifier.padding(0.dp)
         ) {
             PlantImage(
                 imageUrl = plant.imageUrl,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .aspectRatio(16f / 9f).blur(.5.dp)
+                    .aspectRatio(16f / 9f)
+                    .blur(.5.dp)
             )
-            //text will be one line and ellipsized, also CENTERED, have a dark background since the text is white and it will be on top of the image.
 
             Text(
-            text = plant.name,
+                text = plant.name,
                 style = MaterialTheme.typography.titleMedium,
                 modifier = Modifier
                     .background(MaterialTheme.colorScheme.surface)
@@ -66,52 +78,92 @@ fun PlantCardGarden(
             Text(
                 text = "Planted on: ${plant.plantedDate}",
                 style = MaterialTheme.typography.bodyMedium,
-                modifier = Modifier.padding(0.dp).fillMaxWidth(),
+                modifier = Modifier
+                    .padding(0.dp)
+                    .fillMaxWidth(),
                 color = MaterialTheme.colorScheme.onSurface,
                 textAlign = androidx.compose.ui.text.style.TextAlign.Center
             )
             Spacer(modifier = Modifier.height(4.dp))
             //Planted on:
-            //if it has not been watered (lastWateredDate =null) then it will display "Not watered yet"
-
-            val wateredStatus = if (plant.lastWateredDate == null) "Not watered yet" else "Last watered: ${plant.lastWateredDate}"
+//            val wateredStatus = if (plant.lastWateredDate == null) "Not watered yet" else "Last watered: ${plant.lastWateredDate}"
+            val wateredStatus = dateUntilWatering(plant)
             Text(
                 text = wateredStatus,
                 style = MaterialTheme.typography.bodyMedium,
-                modifier = Modifier.padding(4.dp).fillMaxWidth(),
+                modifier = Modifier
+                    .padding(4.dp)
+                    .fillMaxWidth(),
                 color = MaterialTheme.colorScheme.onSurface,
                 textAlign = androidx.compose.ui.text.style.TextAlign.Center
             )
 
+
+            //Row with two buttons: Water and Remove
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = androidx.compose.foundation.layout.Arrangement.SpaceEvenly
+            ) {
+                //Water button
+                TextButton(
+                    onClick = {
+                        viewModel.waterPlant(plant.id)
+                    },
+                    modifier = Modifier
+                        .padding(0.dp)
+                        .fillMaxWidth(1f / 2f)
+                ) {
+                    Text("Water")
+                }
+                //Remove button
+                TextButton(
+                    onClick = {
+                        viewModel.removePlantFromGarden(plant)
+                    },
+                    modifier = Modifier
+                        .padding(0.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text("Remove")
+                }
+
+            }
         }
     }
 }
 
 
+@RequiresApi(Build.VERSION_CODES.O)
+fun dateUntilWatering(plant: Plant): String {
+    plant.lastWateredDate?.let {
 
-//{
-//    "plantId": "malus-pumila",
-//    "name": "Apple",
-//    "description": "An apple is a sweet, edible fruit produced by an apple tree (Malus pumila). Apple trees are cultivated worldwide, and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands of years in Asia and Europe, and were brought to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek and European Christian traditions.<br><br>Apple trees are large if grown from seed. Generally apple cultivars are propagated by grafting onto rootstocks, which control the size of the resulting tree. There are more than 7,500 known cultivars of apples, resulting in a range of desired characteristics. Different cultivars are bred for various tastes and uses, including cooking, eating raw and cider production. Trees and fruit are prone to a number of fungal, bacterial and pest problems, which can be controlled by a number of organic and non-organic means. In 2010, the fruit's genome was sequenced as part of research on disease control and selective breeding in apple production.<br><br>Worldwide production of apples in 2014 was 84.6 million tonnes, with China accounting for 48% of the total.<br><br>(From <a href=\"https://en.wikipedia.org/wiki/Apple\">Wikipedia</a>)",
-//    "growZoneNumber": 3,
-//    "wateringInterval": 30,
-//    "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/5/55/Apple_orchard_in_Tasmania.jpg"
-//  },
+        val lastWatered = LocalDate.parse(it, DateTimeFormatter.ISO_LOCAL_DATE)
+        val nextWateringDate = lastWatered.plusDays(plant.wateringInterval.toLong())
+
+        val today = LocalDate.now()
+
+        // Calculate the days until next watering
+        return if (today.isBefore(nextWateringDate)) {
+            val daysUntilWatering = ChronoUnit.DAYS.between(today, nextWateringDate)
+            "$daysUntilWatering days until watering"
+        } else {
+            "Water me!!!!"
+        }
+    }
+
+    // If the plant hasn't been watered yet
+    return "Not watered yet"
+}
 
 
-
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview
 @Composable
 fun PreviewPlantCardGarden() {
     PlantCardGarden(
-        plant = Plant(
-            id = "malus-pumila",
-            name = "Apple",
-            description = "An apple is a sweet, edible fruit produced by an apple tree (Malus pumila). Apple trees are cultivated worldwide, and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands of years in Asia and Europe, and were brought to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek and European Christian traditions.<br><br>Apple trees are large if grown from seed. Generally apple cultivars are propagated by grafting onto rootstocks, which control the size of the resulting tree. There are more than 7,500 known cultivars of apples, resulting in a range of desired characteristics. Different cultivars are bred for various tastes and uses, including cooking, eating raw and cider production. Trees and fruit are prone to a number of fungal, bacterial and pest problems, which can be controlled by a number of organic and non-organic means. In 2010, the fruit's genome was sequenced as part of research on disease control and selective breeding in apple production.<br><br>Worldwide production of apples in 2014 was 84.6 million tonnes, with China accounting for 48% of the total.<br><br>(From <a href=\"https://en.wikipedia.org/wiki/Apple\">Wikipedia</a>)",
-            growZoneNumber = 3,
-            wateringInterval = 30,
-            imageUrl = "Apple_orchard_in_Tasmania.jpg",
-            plantedDate = "2021-09-01"),
-        modifier = Modifier.padding(8.dp)
+        plantID = "malus-pumila",
+        modifier = Modifier.padding(8.dp),
+        viewModel = {} as GardenViewModel,
     )
 }
+
