a091a3c1d8c09fc690d895f8d9d7754de82065ca
Alex <ruben.ramos51@uabc.edu.mx>
2024-04-28 23:04:20 -0700

    swipeable tabs

--- app/src/main/java/com/example/sunflowerapp/screens/DashboardScreen.kt
+++ app/src/main/java/com/example/sunflowerapp/screens/DashboardScreen.kt
@@ -1,26 +1,44 @@
 package com.example.sunflowerapp.screens
 
+import android.annotation.SuppressLint
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.example.sunflowerapp.PlantRepository
 import com.example.sunflowerapp.composables.GardenGrid
 import com.example.sunflowerapp.composables.PlantList
 import com.example.sunflowerapp.models.Plant
 import com.example.sunflowerapp.viewmodels.GardenViewModel
 
+@SuppressLint("StateFlowValueCalledInComposition", "RememberReturnType")
+@RequiresApi(Build.VERSION_CODES.O)
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun DashboardScreen(viewModel: GardenViewModel, onClick: (Plant) -> Unit) {
+fun DashboardScreen(viewModel: GardenViewModel, onClick: (Plant) -> Unit  ){
     val tabs = listOf("Plants", "Garden")
     val (selectedTabIndex, setSelectedTabIndex) = remember { mutableStateOf(0) }
 
-    val gardenPlants = viewModel.gardenPlants.collectAsState().value
-    val plantList = viewModel.getPlants()
+    val gardenPlants =viewModel.gardenPlants.collectAsState()
+    val plantList = remember { viewModel.getPlants() }
+
+    val pagerState = rememberPagerState {
+        tabs.size
+    }
     Column {
         TabRow(
             selectedTabIndex = selectedTabIndex,
@@ -39,10 +57,29 @@ fun DashboardScreen(viewModel: GardenViewModel, onClick: (Plant) -> Unit) {
                 )
             }
         }
-        // Display the PlantList or GardenScreen Composable based on the selected tab
-        when (selectedTabIndex) {
-            0 -> PlantList(plantList, onClick = onClick)
-            1 -> GardenGrid(gardenPlants, onClick = {setSelectedTabIndex(0)})
+
+        LaunchedEffect(key1 = gardenPlants.value) {
+            // This block will execute every time gardenPlants.value changes,
+            // forcing recomposition of everything inside it.
+            Log.d("DashboardScreen", "Garden plants changed")
+        }
+        LaunchedEffect(selectedTabIndex) {
+            pagerState.animateScrollToPage(selectedTabIndex)
+        }
+
+        LaunchedEffect(pagerState.currentPage) {
+            setSelectedTabIndex(pagerState.currentPage)
+        }
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxHeight()
+        ) { index ->
+            when (index) {
+                0 -> PlantList(plantList, onClick = onClick)
+                1 -> GardenGrid(gardenPlants.value, onClick = { setSelectedTabIndex(0) }, viewModel = viewModel  )
+                else -> throw IllegalStateException("Unexpected index: $index")
+            }
+
         }
     }
 }
